6bbac9ea084c286294bb95307455b52a562452a5
Etapi017 <etapi017@ucr.edu>
2024-05-21 10:25:57 -0700

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#hero">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#education">Education & Skills</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#extracurricular">Extracurricular</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
